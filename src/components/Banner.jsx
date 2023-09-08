d7b4ade6e04984cd2e3dd4701601f04d5dd083bd
Kona <landon.deroin@gmail.com>
2023-09-08 16:03:35 -0400

    added aboutme section

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -13,16 +13,27 @@ const Banner = () => {
           <h1>
             KONA<span> DEROIN</span>
           </h1>
-          <div>
-            <span>I am a </span>
+          <div className="mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]">
+            <span className="text-white mr-4">I am a</span>
             <TypeAnimation
-              sequence={["Developer", 2000, "Designer", 2000, "King", 2000]}
+              sequence={[
+                "Developer",
+                2000,
+                "Designer",
+                2000,
+                "Programmer",
+                2000,
+              ]}
               speed={50}
               className="text-accent"
               wrapper="span"
               repeat={Infinity}
             />
+            
           </div>
+          <p>
+          Recently graduated developer with a passion for cars and coding. When I'm not diving into tech, I'm spending time with my cherished cat or under the hood of an automobile.
+            </p>
         </div>
         <div>
           <img src={Image} alt="Portfolio Picture" />
