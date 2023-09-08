551d4a9db5561107a5c4786cdb3fada57df811f4
Kona <landon.deroin@gmail.com>
2023-09-08 15:35:21 -0400

    added button highlight to nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -16,30 +16,45 @@ const Nav = () => {
         <div className="w-full bg-black/20 h-[96px] backdrop-blur-2x1 rounded-full items-center max-w-[460px] mx-auto px-5 flex justify-between text-2x1 text-white/50">
           <Link
             to="home"
+            activeClass="active"
+            smooth={true}
+            spy={true}
             className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
           >
             <BiHomeAlt />
           </Link>
           <Link
             to="about"
+            activeClass="active"
+            smooth={true}
+            spy={true}
             className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
           >
             <BiUser />
           </Link>
           <Link
             to="skills"
+            activeClass="active"
+            smooth={true}
+            spy={true}
             className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
           >
             <BsClipboardData />
           </Link>
           <Link
             to="work"
+            activeClass="active"
+            smooth={true}
+            spy={true}
             className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
           >
             <BsBriefcase />
           </Link>
           <Link
             to="contact"
+            activeClass="active"
+            smooth={true}
+            spy={true}
             className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
           >
             <BsChatSquare />
