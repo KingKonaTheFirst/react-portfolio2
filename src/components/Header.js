1b8118efa87172fc0083784093232bc62d1c2302
Kona <landon.deroin@gmail.com>
2023-09-08 14:18:55 -0400

    added contact me button and logo change bg too

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,20 @@
-import React from 'react';
-
-
+import React from "react";
+import Logo from '../assets/KonaDeroin.svg'
 const Header = () => {
-  return <div id="home" className='bg-teal-500'>Header</div>;
+  return (
+    <header id="home" className="py-8">
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center ">
+          {/* logo */}
+          <a href="#">
+            <img src={Logo}></img>
+          </a>
+          <button className="btn btn-sm">Contact Me</button>
+        </div>
+
+      </div>
+    </header>
+  );
 };
 
 export default Header;
