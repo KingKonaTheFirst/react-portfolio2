077ef93b8f44092b629cf75f8d88bd6c9b6f8048
Kona <landon.deroin@gmail.com>
2023-09-26 18:27:49 -0400

    did about me section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,21 +15,74 @@ const About = () => {
   return (
     <section id="about" className="section" ref={ref}>
       <div className="container mx-auto">
-        {/* image */}
-        <div className="flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top"></div>
-        {/* text */}
-        <div>
-          <h2>About Me</h2>
-          <h3>
-            Hello! I'm Kona, a web developer recently graduated from a coding
-            bootcamp. I've acquired skills in HTML, CSS, JavaScript, React, and
-            library integrations. Through the bootcamp, I've not only honed my
-            technical abilities but also developed a sharp sense for design and
-            user experience. I'm passionate about turning ideas into dynamic web
-            experiences. Eager to dive into new projects and collaborations,
-            please reach out if you'd like to connect!
-          </h3>
-          {/* stats */}
+        <div className="flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen">
+          {/* image */}
+          <motion.div
+            variants={fadeIn("left", 0.5)}
+            initial="hidden"
+            whileInView="show" // Ensure this works with your setup
+            viewport={{ once: false, amount: 0.3 }} // Ensure this works with your setup
+            className="flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top"
+          ></motion.div>
+          {/* text */}
+          <motion.div
+            variants={fadeIn("right", 0.3)}
+            initial="hidden"
+            whileInView="show" // Ensure this works with your setup
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1"
+          >
+            <h2 className="h2 text-accent">About Me</h2>
+            <h3 className="h3 mb2">
+              I'm a budding Full-Stack Developer looking for a long career in
+              web app development
+            </h3>
+            <p className="mb-6">
+              Hello! I'm Kona, a web developer recently graduated from a coding
+              bootcamp. I've acquired skills in HTML, CSS, JavaScript, React,
+              and library integrations. Through the bootcamp, I've not only
+              honed my technical abilities but also developed a sharp sense for
+              design and user experience. I'm passionate about turning ideas
+              into dynamic web experiences. Eager to dive into new projects and
+              collaborations, please reach out if you'd like to connect!
+            </p>
+            {/* stats */}
+            <div className="flex gap-x-6 lg:gap-x-10 mb-12">
+              <div>
+                <div className="text-[40px] font-tertiary text-gradient mb-2">
+                  {inView ? <CountUp start={0} end={1} duration={3} /> : null}
+                </div>
+                <div className="font-primary text-sm tracking-[2px]">
+                  Years of <br />
+                  Experience
+                </div>
+              </div>
+              <div>
+                <div className="text-[40px] font-tertiary text-gradient mb-2">
+                  {inView ? <CountUp start={0} end={15} duration={3} /> : null}
+                </div>
+                <div className="font-primary text-sm tracking-[2px]">
+                  Projects <br />
+                  Completed
+                </div>
+              </div>
+              <div>
+                <div className="text-[40px] font-tertiary text-gradient mb-2">
+                  {inView ? <CountUp start={0} end={1} duration={3} /> : null}
+                </div>
+                <div className="font-primary text-sm tracking-[2px]">
+                  Job <br />
+                  Completed
+                </div>
+              </div>
+            </div>
+            <div className="flex gap-x-8 items-center">
+              <button className="btn btn-lg">Contact Me</button>
+              <a href="#" className="text-gradient btn-link">
+                My Projects
+              </a>
+            </div>
+          </motion.div>
         </div>
       </div>
     </section>
